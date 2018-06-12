18ecd817fcfc1f13f43b6f31314dbd8d38622c9a
zhuangmeili <zhuangmeili@auohome.com.cn>
2018-06-12 14:23:00 +0800

    [add] webpack 基础配置

--- src/index.js
+++ src/index.js
@@ -1,14 +1,12 @@
 /**
  * 入口文件
- *
  * */
-
 function component(){
   var ele=document.createElement('div');
-  ele.innerHTML=_.join('Hello','webpack', ' ');
+  ele.innerHTML=['Hello','webpack','北京欢迎你'].join(' ');
+
   return ele;
 }
-
 document.body.appendChild(component());
 
 
