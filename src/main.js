f72fd0f4cac0d9f39000e01fe607e6e4b618916e
zhuangmeili <zhuangmeili@auohome.com.cn>
2018-06-12 15:54:20 +0800

    [add]css and img资源加载

--- src/main.js
+++ src/main.js
@@ -1,9 +1,8 @@
-/**
- * 入口文件
- * */
+import './App.css';
 function component(){
   var ele=document.createElement('div');
   ele.innerHTML=['Hello','webpack','北京欢迎你'].join(' ');
+  ele.classList.add('hello');
 
   return ele;
 }
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,29 @@
 const path=require('path');
 
+
 module.exports={
-  entry: "./src/index.js",
+  entry: "./src/main.js",
   output: {
     filename: "bundle.js",
     path: path.resolve(__dirname,'dist')
+  },
+  module: {
+    rules: [
+      {
+        test:/.css$/,
+        use:['style-loader','css-loader']
+      },
+      {
+        test:/\.(jpg|jpeg|png|gif|svg)$/,
+        use: [
+          {
+            loader: "url-loader",
+            options: {
+              limit:1000
+            }
+          }
+        ]
+      }
+    ]
   }
 }
